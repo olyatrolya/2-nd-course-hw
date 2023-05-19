36ea2aa8240d5dfad45f3963f2555af73ce5f9cf
olyatrolya <kaferrchen@mail.ru>
2023-05-19 22:43:32 +0300

    ex1-4

---
+++ hw-2.js
@@ -0,0 +1,20 @@
+/* 
+let a = 20;
+alert (a);
+
+let releaseyear = 2007 // год выпуска первого iPhone
+alert (releaseyear)
+
+let jsfounder = "Brendon Eich"; //имя создателя языка JavaScript
+alert (jsfounder);
+*/
+let fingers = Number(prompt("Сколько пальцев на руках?")); //10
+let fingerschange = 2;
+/* let result = fingers + fingerschange;
+alert (`А у людей с полидактилией может быть ${result}`);*/
+/* let result = fingers - fingerschange;
+alert (`Если ты предашь главу мафии, после юбицумэ у тебя останется ${result}`);*/
+/*let result = fingers*fingerschange;
+alert (`Если к ним прибавить пальцы ног, то получится ${result}`);*/
+let result = fingers/fingerschange;
+alert (`Если ты поделишься одной рукой с другом, то останется ${result}`);
