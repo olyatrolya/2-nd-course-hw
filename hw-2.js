0faecbee25d9c1b4ff453639a0f254db17192fcb
olyatrolya <kaferrchen@mail.ru>
2023-05-20 22:20:45 +0300

    rest ex

--- hw-2.js
+++ hw-2.js
@@ -1,5 +1,7 @@
-/* 
-let a = 20;
+
+let a = 10;
+alert (a);
+a = 20;
 alert (a);
 
 let releaseyear = 2007 // год выпуска первого iPhone
@@ -10,25 +12,41 @@ alert (jsfounder);
 
 let fingers = Number(prompt("Сколько пальцев на руках?")); //10
 let fingerschange = 2;
-/* let result = fingers + fingerschange;
-alert (`А у людей с полидактилией может быть ${result}`);*/
-/* let result = fingers - fingerschange;
-alert (`Если ты предашь главу мафии, после юбицумэ у тебя останется ${result}`);*/
-/*let result = fingers*fingerschange;
-alert (`Если к ним прибавить пальцы ног, то получится ${result}`);*/
-/* let result = fingers/fingerschange;
-alert (`Если ты поделишься одной рукой с другом, то останется ${result}`);
+alert (`А у людей с полидактилией может быть ${fingers + fingerschange}`);
+alert (`Если ты предашь главу мафии, после юбицумэ у тебя останется ${fingers - fingerschange}`);
+alert (`Если к ним прибавить пальцы ног, то получится ${fingers*fingerschange}`);
+alert (`Если ты поделишься одной рукой с другом, то останется ${fingers/fingerschange}`);
 
 let problemswithjs=2;
 let resultProblems=2**5;
 alert(resultProblems);
 
-let a = 9; b = 2;
-let resultAb = a%b;
-alert(resultAb);
-*/
+let a2 = 9; b = 2;
+alert(a2%b);
 
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+++num;
+--num;
+alert(num);
 
 let age = Number(prompt("Сколько тебе лет?"));
 alert(age);
 
+const user = {
+    userName: "IhateJS",
+    age: 100500,
+    isAdmin: false,
+};
+user.cityOfResidence = "Kongo";
+delete user.cityOfResidence;
+user.age = 1;
+let info = prompt("Какую информацию хотите узнать о пользователе?","userName","age");
+alert(user[info]);
+
+
+let yourname = prompt("Как тебя на районе называют?");
+alert(`Привет `+ yourname +` !`);
