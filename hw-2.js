db20c95b7e72b8673b08b2426964fa64cb150817
olyatrolya <kaferrchen@mail.ru>
2023-05-20 18:19:36 +0300

    5, 6, 8

--- hw-2.js
+++ hw-2.js
@@ -7,7 +7,7 @@ alert (releaseyear)
 
 let jsfounder = "Brendon Eich"; //имя создателя языка JavaScript
 alert (jsfounder);
-*/
+
 let fingers = Number(prompt("Сколько пальцев на руках?")); //10
 let fingerschange = 2;
 /* let result = fingers + fingerschange;
@@ -16,5 +16,19 @@ alert (`А у людей с полидактилией может быть ${res
 alert (`Если ты предашь главу мафии, после юбицумэ у тебя останется ${result}`);*/
 /*let result = fingers*fingerschange;
 alert (`Если к ним прибавить пальцы ног, то получится ${result}`);*/
-let result = fingers/fingerschange;
+/* let result = fingers/fingerschange;
 alert (`Если ты поделишься одной рукой с другом, то останется ${result}`);
+
+let problemswithjs=2;
+let resultProblems=2**5;
+alert(resultProblems);
+
+let a = 9; b = 2;
+let resultAb = a%b;
+alert(resultAb);
+*/
+
+
+let age = Number(prompt("Сколько тебе лет?"));
+alert(age);
+
