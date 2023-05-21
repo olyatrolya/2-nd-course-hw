7ea157c487704e2d128ed9b7f8ea15cea3a715fa
olyatrolya <kaferrchen@mail.ru>
2023-05-21 11:38:17 +0300

    fix

--- hw-2.js
+++ hw-2.js
@@ -1,11 +1,11 @@
 
-let a = 10;
+/*let a = 10;
 alert (a);
 a = 20;
 alert (a);
 
-let releaseyear = 2007 // год выпуска первого iPhone
-alert (releaseyear)
+let IPhone1ReleaseYear = "Год выпуска первого iPhone - " +2007;// год выпуска первого iPhone
+alert (IPhone1ReleaseYear)
 
 let jsfounder = "Brendon Eich"; //имя создателя языка JavaScript
 alert (jsfounder);
@@ -34,17 +34,17 @@ num = num / 3;
 alert(num);
 
 let age = Number(prompt("Сколько тебе лет?"));
-alert(age);
+alert(age);*/
 
 const user = {
     userName: "IhateJS",
     age: 100500,
     isAdmin: false,
 };
-user.cityOfResidence = "Kongo";
-delete user.cityOfResidence;
+user["city of residence"] = "Kongo";
+delete user["city of residence"];
 user.age = 1;
-let info = prompt("Какую информацию хотите узнать о пользователе?","userName","age");
+let info = prompt("Какую информацию хотите узнать о пользователе?","userName","age","city of residence");
 alert(user[info]);
 
 
