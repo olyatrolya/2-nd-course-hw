6b990d212f1a23bdfa91415fbf3d7e4b65dce02d
olyatrolya <kaferrchen@mail.ru>
2023-05-23 11:38:00 +0300

    HW3 EX 1-3

---
+++ hw-3.js
@@ -0,0 +1,23 @@
+/*
+let password = prompt ('Введите пароль');
+if (password === 'olyaisthemostbeautifulgirl') {
+    alert ('Пароль введен верно')
+} else {
+    alert ('Пароль введен неправильно')
+}
+*/
+let c = 3;
+if (c > 0 && c < 10) {
+    console.log ('верно')
+} else {
+    console.log ('неверно')
+}
+
+let d = 1066;
+let e = 13;
+if (d > 100 || e > 100) {
+    console.log ('верно')
+} else {
+    console.log ('неверно')
+}
+
