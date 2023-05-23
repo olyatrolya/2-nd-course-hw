1e863942686186c66780766dce3889ea676d8d8f
olyatrolya <kaferrchen@mail.ru>
2023-05-23 21:18:07 +0300

    hover

--- hw-3.js
+++ hw-3.js
@@ -1,11 +1,11 @@
-/*
+
 let password = prompt ('Введите пароль');
 if (password === 'olyaisthemostbeautifulgirl') {
     alert ('Пароль введен верно')
 } else {
     alert ('Пароль введен неправильно')
 }
-*/
+
 let c = 3;
 if (c > 0 && c < 10) {
     console.log ('верно')
@@ -21,3 +21,32 @@ if (d > 100 || e > 100) {
     console.log ('неверно')
 }
 
+let a = '2';
+let b = '3';
+alert(Number(a) + Number(b));
+
+let monthNumber = prompt('Введите номер месяца');
+switch (monthNumber){
+    case '1':
+    case '2':
+    case '12':
+        console.log('зима');
+    break;
+    case '3':
+    case '4':
+    case '5':
+        console.log('весна');
+    break;
+    case '6':
+    case '7':
+    case '8':
+        console.log('лето');
+    break;
+    case '9':
+    case '10':
+    case '11':
+        console.log('осень');
+    break;
+default: console.log('Такого не существует');
+    break;
+}
