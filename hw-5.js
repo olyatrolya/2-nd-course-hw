a3d84053d5004eec1d95800c295a3b6a08954b7a
olyatrolya <kaferrchen@mail.ru>
2023-05-31 21:52:18 +0300

    js all tasks

--- hw-5.js
+++ hw-5.js
@@ -1,3 +1,6 @@
+/*
+//Задание №1
+
 function minNumber(a,b) {
     if (a < b)
     return a;
@@ -7,6 +10,18 @@ function minNumber(a,b) {
 }
 console.log(minNumber(8, 4))
 console.log(minNumber(6, 6))
+
+const showMinNumber = (a, b) => {
+  if (a < b) {
+    return a;
+  } else {
+      return b;
+    }
+  }
+  console.log(showMinNumber(8, 4))
+  console.log(showMinNumber(6, 6))
+
+//Задание №2
 
 function parityCheck (num) {
     if (num % 2 === 0)
@@ -18,16 +33,21 @@ function parityCheck (num) {
 console.log(parityCheck(30));
 console.log(parityCheck(67));
 
-function square (chislo) {
-    console.log(chislo * chislo);
+//Задание №3
+
+function clacSquare (x) {
+    console.log(x * x);
 };
-square(5);
-function kvadrat (n) {
+clacSquare(5);
+
+function showSquare (n) {
     return n * n
 };
-console.log(kvadrat(8));
+console.log(showSquare(8));
 
-function age (question) {
+//Задание №4
+
+function sayHello (question) {
     let userAnswer = Number(prompt(question));
     if (userAnswer < 0) {
     console.log('Вы ввели неправильное значение');
@@ -39,4 +59,90 @@ function age (question) {
             console.log('Добро пожаловать!');
         }
     }
-age('Сколько тебе лет?');
+sayHello('Сколько тебе лет?');
+
+//Задание №5
+
+function chekNumber(z,v) {
+    if (isNaN(z) || isNaN(v))
+    console.log('Одно или оба значения не являются числом');
+    else {
+      console.log(z * v);
+    }
+  }
+  chekNumber(9,7)
+
+//Задание №6   
+
+  function question () {
+    let userAnswer = prompt('Введите число');
+    if (isNaN(userAnswer))
+    return alert('Переданный параметр не является числом');
+    else {
+      let userAnswerVKube = userAnswer**3;
+      return alert(userAnswerVKube);
+    }
+  }
+  question()
+
+  //Задание №7
+  
+  function getCircleArea() {
+    return (Math.PI) * this.radius ** 2;
+  }
+  function getCirclePerimeter() {
+    return 2 * (Math.PI) * this.radius;
+  }
+   
+  const circle1 = {
+    radius: 5,
+   
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter,
+  };
+  const circle2 = {
+    radius: 9,
+   
+    getArea: getCircleArea,
+    getPerimeter: getCirclePerimeter, 
+  };
+  console.log(circle1.getArea());
+  console.log(circle1.getPerimeter());
+  console.log(circle2.getArea());
+  console.log(circle2.getPerimeter());
+   */
+  //Задание №8
+
+
+ 
+  function saySeason () {
+    let userAnswer = Number(prompt('Введи номер месяца'));
+    if (userAnswer === 12 || userAnswer === 1 || userAnswer === 2) 
+        return alert('зима');
+     else if (userAnswer === 3 || userAnswer === 4 || userAnswer === 5) 
+        return alert('весна');
+     else if (userAnswer === 6 || userAnswer === 7 || userAnswer === 8) 
+        return alert('лето');
+     else if (userAnswer === 9 || userAnswer === 10 || userAnswer === 11) 
+        return alert('осень');
+     else {
+        return alert('нет такого');
+    }
+  }
+ 
+
+ /*
+  const saySeason = () => {
+    if (s === 12 || s === 1 || s === 2) {
+    return 'зима';
+    } else if (s === 3 || s === 4 || s === 5) {
+    return 'весна';
+    } else if (s === 6 || s === 7 || s === 8) {
+    return 'лето';
+    } else if (s === 9 || s === 10 || s === 11) {
+    return 'осень';
+    } else {
+        return 'нет такого';
+    }
+  }
+   */
