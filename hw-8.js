8cbcc27d7c3d995a6362f0813bca0108b7513a95
olyatrolya <kaferrchen@mail.ru>
2023-06-10 10:27:38 +0300

    8 created

---
+++ hw-8.js
@@ -0,0 +1,20 @@
+// Задание №1
+
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+ ];
+ people.sort(function name(params) {
+    
+ })
+ // Допишите колбэк для sort, изучите, как работает колбэк, в документации
+ console.log(people.sort(...));
+ // код выше должен вывеcти =>
+ // [
+ //  { name: 'Олег', age: 7 },
+ //  { name: 'Анна', age: 17 },
+ //  { name: 'Глеб', age: 29 },
+ //  { name: 'Оксана', age: 47 }
+ // ]
