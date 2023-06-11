d9524dbee5d74c1794393718cea1eca1e409d3b7
olyatrolya <kaferrchen@mail.ru>
2023-06-11 22:56:38 +0300

    tasks js

--- hw-8.js
+++ hw-8.js
@@ -1,20 +1,56 @@
 // Задание №1
-
 const people = [
     { name: 'Глеб', age: 29 },
     { name: 'Анна', age: 17 },
     { name: 'Олег', age: 7 },
     { name: 'Оксана', age: 47 }
  ];
- people.sort(function name(params) {
+console.log(people.sort((prev, next) => prev.age - next.age));
+
+// Задание №2
+
+
+function isPositive(arr) {
+    return arr.filter(number => typeof number == 'number' && number >= 0);
+  }
+  console.log(isPositive([3, -4, 1, 9]));
+
+
+
+// Задание №3
+
+const date = setInterval(() => console.log(new Date()), 1000 * 3);
+setTimeout(() => { clearInterval(date); console.log('30 секунд прошло'); }, 1000 * 30);
+
+// Задание №4
+
+function delayForSecond() {
+    console.log('Привет, Глеб!')
+}
+    setTimeout(delayForSecond, 1000)
+
+    setTimeout(function() {
+        console.log('Привет, Глеб!');
+    }, 1000);
+
     
- })
- // Допишите колбэк для sort, изучите, как работает колбэк, в документации
- console.log(people.sort(...));
- // код выше должен вывеcти =>
- // [
- //  { name: 'Олег', age: 7 },
- //  { name: 'Анна', age: 17 },
- //  { name: 'Глеб', age: 29 },
- //  { name: 'Оксана', age: 47 }
- // ]
+
+// Задание №5
+
+
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) { cb(); }
+    }, 1000)
+}
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecond(() => sayHi('Глеб'));
+
+
+
+
